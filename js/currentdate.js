e3e3317875f9c7fb989f47176a67d934e37e4954
Thiagogl14 <u838651204@byui.edu>
2022-01-14 20:36:35 -0300

    Home Page Update

--- js/currentdate.js
+++ js/currentdate.js
@@ -1,2 +1,13 @@
-const options = {weekday: 'long', day:'numeric', month: 'long', year: 'numeric'};
-document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
+
+document.getElementById("year").innerHTML = new Date().getFullYear()
+
+var temp = document.lastModified
+var n = parseInt(temp.substring(temp.length-8,temp.length-6))
+if (n>12) {
+    document.getElementById("updated").innerHTML = "Last Updated: "+
+        temp.substring(0,temp.length-8)+(n-12)+temp.substring(temp.length-6)+" pm"
+}
+else {
+    document.getElementById("updated").innerHTML = "Last Updated: "+
+        temp+" am"
+}
