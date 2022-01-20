0f8bd5e3124de3316bd5622f03dfaecb57ebf586
Thiagogl14 <u838651204@byui.edu>
2022-01-19 22:59:20 -0300

    Commerce Checkpoint - Site Plan

---
+++ Lesson3/js/currentdate.js
@@ -0,0 +1,13 @@
+
+document.getElementById("year").innerHTML = new Date().getFullYear()
+
+var temp = document.lastModified
+var n = parseInt(temp.substring(temp.length-8,temp.length-6))
+if (n>12) {
+    document.getElementById("updated").innerHTML = "Last Updated: "+
+        temp.substring(0,temp.length-8)+(n-12)+temp.substring(temp.length-6)+" pm"
+}
+else {
+    document.getElementById("updated").innerHTML = "Last Updated: "+
+        temp+" am"
+}
