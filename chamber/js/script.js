204ba72378551ed1271ae360c0f7f1964646d5d6
Thiagogl14 <u838651204@byui.edu>
2022-01-27 11:56:04 -0300

    Home Page

---
+++ chamber/js/script.js
@@ -0,0 +1,27 @@
+
+document.getElementById("year").innerHTML = new Date().getFullYear()
+
+var temp = document.lastModified
+var n = parseInt(temp.substring(temp.length-8,temp.length-6))
+if (n>12) {
+    document.getElementById("updated").innerHTML = "Last Updated: "+
+        temp.substring(0,temp.length-8)+(n-12)+temp.substring(temp.length-6)+" pm"
+}
+else {
+    document.getElementById("updated").innerHTML = "Last Updated: "+
+        temp+" am"
+}
+
+let now = new Date()
+let months = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]
+let weekDays = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ]
+document.querySelector('footer .current-date').innerHTML = weekDays[now.getDay()]+", "
+                                                          +months[now.getMonth()]+" "
+                                                          +now.getDate()+", "
+                                                          +now.getFullYear()
+function toggleMenu() {
+    var temp = document.getElementsByClassName("toggler")[0]
+    temp.classList.toggle("hide")
+    if (temp.classList[1]=="hide") document.querySelector(".toggler > a").innerHTML = "&#9776; Menu"
+    else document.querySelector(".toggler > a").innerHTML = "&#10006; Close"
+                        }
