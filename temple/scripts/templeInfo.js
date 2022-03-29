7d0f60d97e0d49317250cb9bae6da8985a0bc022
Thiagogl14 <u838651204@byui.edu>
2022-03-28 21:19:06 -0300

    Temple Project

--- temple/scripts/templeInfo.js
+++ temple/scripts/templeInfo.js
@@ -1,62 +1,64 @@
-const templeInfoURL = 'https://thiagogl14.github.io/wdd230/temple/data/closureInfo.json';
-let templeInfoRequest = new XMLHttpRequest();
-templeInfoRequest.open('GET', templeInfoURL);
-templeInfoRequest.responseType = 'json';
-templeInfoRequest.send();
-templeInfoRequest.onload = function () {
-    let templeData = templeInfoRequest.response;
-    console.log(templeData);
-    let temples = templeData['temples'];
-    let d = "";
-    var str = "";
-    temples.forEach(temple => {
-        switch (temple.templename) {
-            case "Boston":
-                for (let i = 0; i < temple.closuredates.length; i++) {
-                    d = temple.closuredates[i];
-                    str = d.split(":");
-                    document.getElementById("bostonClosed").innerHTML += "&#9656;" + str + "<br>";
-                }
-                break;
-            case "Hartford":
-                for (let i = 0; i < temple.closuredates.length; i++) {
-                    d = temple.closuredates[i];
-                    str = d.split(":");
-                    document.getElementById("hartfordClosed").innerHTML += "&#9656;" + str + "<br>";
-                }
-                break;
-            case "Manhattan":
-                for (let i = 0; i < temple.closuredates.length; i++) {
-                    d = temple.closuredates[i];
-                    str = d.split(":");
-                    document.getElementById("manhattanClosed").innerHTML += "&#9656;" + str + "<br>";
-                }
-                break;
-            case "Philadelphia":
-                for (let i = 0; i < temple.closuredates.length; i++) {
-                    d = temple.closuredates[i];
-                    str = d.split(":");
-                    document.getElementById("philadelphiaClosed").innerHTML += "&#9656;" + str + "<br>";
-                }
-                break;
-            case "Raleigh":
-                for (let i = 0; i < temple.closuredates.length; i++) {
-                    d = temple.closuredates[i];
-                    str = d.split(":");
-                    document.getElementById("raleighClosed").innerHTML += "&#9656;" + str + "<br>";
-                }
-                break;
-            case "Columbia":
-                for (let i = 0; i < temple.closuredates.length; i++) {
-                    d = temple.closuredates[i];
-                    str = d.split(":");
-                    document.getElementById("columbiaClosed").innerHTML += "&#9656;" + str + "<br>";
-                }
-                break;
-            default:
-                break;
+const templeURL = 'https://thiagogl14.github.io/wdd230/temple/data/templeInfo.json';
+fetch(templeURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        let information = jsObject.temples;
+        for (const temple in information) {
+            let card = document.createElement('section');
+            let text = document.createElement('div');
+            card.appendChild(text);
+            card.classList.add('temple-card');
+            let cardContent = information[temple];
+
+            let h3 = document.createElement('h3');
+            h3.textContent = cardContent.name;
+            h3.classList.add('card-title');
+            text.appendChild(h3);
+
+            let address = document.createElement('p');
+            address.textContent = cardContent.address;
+            address.classList.add('card-address');
+            text.appendChild(address);
+
+            let phone = document.createElement('p');
+            phone.textContent = cardContent.phone;
+            phone.classList.add('card-number');
+            text.appendChild(phone);
+
+            let services = document.createElement('p');
+            services.textContent = cardContent.services;
+            services.classList.add('card-services');
+            text.appendChild(services);
+
+            let email = document.createElement('p');
+            email.textContent = cardContent.email;
+            email.classList.add('card-email');
+            text.appendChild(email);
+
+            let history = document.createElement('p');
+            history.textContent = cardContent.history;
+            history.classList.add('card-history');
+            text.appendChild(history);
+
+            let schedule = document.createElement('p');
+            schedule.textContent = cardContent.schedule;
+            schedule.classList.add('card-schedule');
+            text.appendChild(schedule);
+
+            let closure = document.createElement('p');
+            closure.textContent = cardContent.closure;
+            closure.classList.add('card-closure');
+            text.appendChild(closure);
+
+            if (cardContent.logo != "") {
+                let logo = document.createElement('img');
+                logo.setAttribute('src', cardContent.logo);
+                logo.setAttribute('alt', cardContent.name);
+                logo.classList.add('card-img');
+                logo.setAttribute('loading', 'lazy');
+                card.appendChild(logo);
+            }
+
+            document.querySelector('.temple').appendChild(card);
         }
     });
-
-
-};
